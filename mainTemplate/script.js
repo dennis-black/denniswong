6f425b73e8f8f990fc74c9fd995656aef5ba7e94
dennis <cbb111227@nptu.edu.tw>
2025-01-30 21:08:45 +0800

    correct ref

--- mainTemplate/script.js
+++ mainTemplate/script.js
@@ -1,7 +1,7 @@
-document.getElementById("headerSection").innerHTML = fetch("./mainTemplate/header.html")
+document.getElementById("headerSection").innerHTML = fetch("/mainTemplate/header.html")
             .then(response => response.text())
             .then(data => document.getElementById("headerSection").innerHTML = data);
 
-document.getElementById("footerSection").innerHTML = fetch("./mainTemplate/footer.html")
+document.getElementById("footerSection").innerHTML = fetch("/mainTemplate/footer.html")
     .then(response => response.text())
     .then(data => document.getElementById("footerSection").innerHTML = data);
